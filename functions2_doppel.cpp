39890c76391ca8974aa8abb9d26137e392a723ae
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-20 13:22:56 +0600

    Add files via upload

---
+++ functions2_doppel.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+void func_power(int value, int power);
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    
+    func_power(5, 2);
+    func_power(3, 3);
+    func_power(4, 4);
+
+
+    return 0;
+}
+
+void func_power(int value, int power) {
+    int result = 1;
+    for (int i = 0; i < power; ++i)
+    {
+        result *= value;
+    }
+    cout << value << " в степени " << power << " = " << result << endl;
+}
+
+
